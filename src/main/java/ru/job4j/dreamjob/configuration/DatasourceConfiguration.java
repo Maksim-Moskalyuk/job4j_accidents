5dc0575c04e9df7a886a4806f55724b5f847d6aa
Maksim-Moskalyuk <maksim.moskalyuk.94@gmail.com>
2023-04-06 01:37:32 +0300

    1. Техническое задание - проект "Автонарушители" [#260365]

---
+++ src/main/java/ru/job4j/dreamjob/configuration/DatasourceConfiguration.java
@@ -0,0 +1,26 @@
+package ru.job4j.dreamjob.configuration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfiguration {
+
+    @Bean
+    public DataSource connectionPool(@Value("${datasource.url}") String url,
+                                     @Value("${datasource.username}") String username,
+                                     @Value("${datasource.password}") String password) {
+        return new BasicDataSource() {
+            {
+                setUrl(url);
+                setUsername(username);
+                setPassword(password);
+            }
+        };
+    }
+
+}
